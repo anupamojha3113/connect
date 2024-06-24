8a83835c5a27db5eae32a6a2ac73c94a7b35fd42
Anupam Ojha <anupamojha3113@gmail.com>
2024-06-24 09:50:21 +0530

    Atlas connected successfully

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import express from 'express'
-import dotenv from 'dotenv'
-const app=express();
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
 
-dotenv.config({
-    path:'./env'
-})
+dotenv.config({ path: './env' });
+
+const app = express();
 app.use(express.json());
 
-app.listen(process.env.PORT||8000,()=>{
-    console.log(`Server is running at post : ${process.env.PORT}`);
-})
+connectDB().then(() => {
+    console.log('Database connected, server can start now');
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    });
+}).catch(error => {
+    console.error('Failed to connect to the database', error);
+});
